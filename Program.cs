55401ead89b8a963be9b8462a6dff93551550848
Schub Bergman <maks.kon.92@mail.ru>
2023-11-27 15:44:36 +0100

    Task on params

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+namespace ConsoleApplication3
+{
+    internal class Program
+    {
+        static void Foo(params object[] parametres)
+        {
+            string message = "Type of data {0}, value{1}";//2 из за этого
+            foreach (var item in parametres)
+            {
+                Console.WriteLine(message,item.GetType(),item);//1то есть принимает переменную а потом две подпеременные из парамса
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Foo("test",5,6,2,3);
+            object o = 5;
+            object a = 55;
+        }
+    }
+}
